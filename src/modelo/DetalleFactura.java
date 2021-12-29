afbbcf1e3fe4cefca361140adfed6a57b6a7c5f6
John <John@DESKTOP-JLTIKLK>
2021-12-29 15:04:36 -0500

    añadido clase detalleFactura

---
+++ src/modelo/DetalleFactura.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelo;
+
+/**
+ *
+ * @author John
+ */
+public class DetalleFactura {
+
+    private Long Id;
+    private String descripcionProducto;
+    private int cantidad;
+    private double precioTotal;
+    
+    public Long getId() {
+        return Id;
+    }
+
+    public void setId(Long Id) {
+        this.Id = Id;
+    }
+
+    public String getDescripcionProducto() {
+        return descripcionProducto;
+    }
+
+    public void setDescripcionProducto(String descripcionProducto) {
+        this.descripcionProducto = descripcionProducto;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public double getPrecioTotal() {
+        return precioTotal;
+    }
+
+    public void setPrecioTotal(double precioTotal) {
+        this.precioTotal = precioTotal;
+    }
+}
