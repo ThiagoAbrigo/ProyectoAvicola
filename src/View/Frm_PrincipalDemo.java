3e2d35a89462605df739185e734f9c4b941df92f
Home <Home@LAPTOP-A3A8UVMK>
2022-01-28 00:14:11 -0500

    avances en login, galpon y vacuna

--- src/View/Frm_PrincipalDemo.java
+++ src/View/Frm_PrincipalDemo.java
@@ -3,7 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JDialog.java to edit this template
  */
 package View;
-//
+///
 import Controller.GalponController;
 import Controller.VacunaController;
 import View.Table.TableGalpon;
